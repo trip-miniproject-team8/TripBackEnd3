cea8d7a03e1390585212f7091dbcfc0d12f6e152
bigtyno931128 <bigtyno4@gmail.com>
2022-04-08 12:13:31 +0900

    test

---
+++ src/main/java/com/example/tripbackend3/TripBackEnd3Application.java
@@ -0,0 +1,13 @@
+package com.example.tripbackend3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripBackEnd3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripBackEnd3Application.class, args);
+    }
+
+}

3cdde450b3e6a8e6c3aed9f616108c52e7b6cfd1
bigtyno931128 <bigtyno4@gmail.com>
2022-04-10 00:54:04 +0900

    로그인

---
+++ src/main/java/com/example/tripbackend3/controller/HomeController.java
@@ -0,0 +1,24 @@
+//package com.example.tripbackend3.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//public class HomeController {
+//
+//    // 회원 로그인 페이지
+//    @GetMapping("/user/login")
+//    public String login() {
+//
+//        return "/login.html";
+//    }
+//
+//
+//
+//    // 회원 가입 페이지
+//    @GetMapping("/user/signup")
+//    public String signup() {
+//        return "/signup.html";
+//    }
+//}
